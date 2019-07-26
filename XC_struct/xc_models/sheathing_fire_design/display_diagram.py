8694aad90ebd2438fd012218d55dfdabb9a90d3b
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-07-26 11:44:08 +0200

    Fix typo

---
+++ XC_struct/xc_models/sheathing_fire_design/display_diagram.py
@@ -0,0 +1,24 @@
+
+execfile('./xc_model.py')
+
+#Graphic output
+from postprocess.xcVtk.FE_model import quick_graphics as QGrph
+from postprocess.reports import graphical_reports as gr
+from postprocess.xcVtk import vtk_graphic_base
+
+lc= gr.getRecordLoadCaseDispFromLoadPattern(cLC)
+lc.setsToDispDspRot=[infSet]#[overallSet]
+lc.setsToDispIntForc=[supSet]
+lc.unitsScaleLoads=1e-3
+lc.unitsScaleDispl=1e3
+lc.unitsDispl='[mm]'
+lc.unitsScaleMom=1e-3
+lc.unitsMom='[m.kN]'
+lc.unitsScaleForc=1e-3
+lc.unitsForc='[kN]'
+lc.listDspRot=['uY']
+lc.cameraParameters= vtk_graphic_base.CameraParameters('ZPos')
+
+lc.displayIntForcDiag(itemToDisp= 'Mz')
+
+
