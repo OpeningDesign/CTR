a7f463ca2e1878cce2c3d4cdb6242bffd7c165c1
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-05-12 19:33:15 +0200

    Update code

--- XC_struct/xc_models/footings_model/calc_internalForces_ULS.py
+++ XC_struct/xc_models/footings_model/calc_internalForces_ULS.py
@@ -40,5 +40,5 @@ lsd.fatigueResistance # Fatigue resistance.
 ] 
 
 for ls in limitStates:
-  ls.saveAll(feProblem= FEproblem,combContainer=combContainer,setCalc= elementsWithSection,fConvIntForc= 1.0)
+  ls.saveAll(combContainer=combContainer,setCalc= elementsWithSection,fConvIntForc= 1.0)
   print('combinations for ', ls.label, ': ', loadCombinations.getKeys())
